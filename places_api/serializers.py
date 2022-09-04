99865453cb12dc21936c065ed5d43625563dd475
JosueReyes-1 <josueisrae222@gmail.com>
2022-09-04 16:54:40 +0000

    serializer creation for Favorite Place, Img Place, Category model

--- places_api/serializers.py
+++ places_api/serializers.py
@@ -1,16 +1,31 @@
 
+from itertools import count
 from rest_framework import serializers
 
-from .models import Place
+from .models import Category, FavoritePlaces, ImgPlace, Place
 
 
+class CategorySerializerModel(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
 
 
+class ImgPlaceSerializerModel(serializers.ModelSerializer):
+    class Meta:
+        model = ImgPlace
+        fields = ['img']
+
+class FavoritePlaceSerializarModel(serializers.ModelSerializer):
+    class Meta:
+        model = FavoritePlaces
+        fields= ['status']
 
 class PlaceSerializerModel(serializers.ModelSerializer):
-    images=serializers.StringRelatedField(many=True)
-    category=serializers.CharField(source='category.name')
+    category=CategorySerializerModel()
+    images=ImgPlaceSerializerModel(many=True)
+    fav=FavoritePlaceSerializarModel(many=True)
     class Meta:
         model= Place
-        fields = ('name','description','category','country','state','city','images')
+        fields = ('name','description','category','country','state','city','images','fav')
 
