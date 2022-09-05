d47149bf97b82e2a3fed176202ea1bce7fa9fca1
JosueReyes-1 <josueisrae222@gmail.com>
2022-09-05 17:43:04 +0000

    added a queryset

--- places_api/views.py
+++ places_api/views.py
@@ -1,13 +1,13 @@
 from django.http import JsonResponse
-from places_api.additionalFunctions import QueryData
 from places_api.serializers import PlaceSerializerModel
 from .models import FavoritePlaces, Place,ImgPlace
 from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.generics import ListAPIView
 
 from django_filters.rest_framework import DjangoFilterBackend
-
+from django.db.models import Count
 class SearchApiView(ListAPIView):
+    
     queryset=Place.objects.all()
     serializer_class=PlaceSerializerModel
     filter_backends=(SearchFilter,OrderingFilter,DjangoFilterBackend)
