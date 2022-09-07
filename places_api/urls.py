3f790a2b321adcd10dccfe38e45c42f62a4a9895
JosueReyes-1 <josueisrae222@gmail.com>
2022-09-07 18:13:14 +0000

    Added new favorite place URLs

--- places_api/urls.py
+++ places_api/urls.py
@@ -1,7 +1,10 @@
+from unicodedata import name
 from django.urls import path
 
 from places_api import views
 
 urlpatterns = [
     path('place/',views.SearchApiView.as_view()),
+    path('favorite/',views.FavoriteApiView),
+    path('favorite/<int:pk>',views.FavoriteApiView),
 ]
