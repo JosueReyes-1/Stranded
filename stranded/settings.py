291244aecd82913afa019198045f14af7874fec6
JosueReyes-1 <josueisrae222@gmail.com>
2022-09-24 03:08:04 +0000

    make it better

--- stranded/settings.py
+++ stranded/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sht!%n3(4h#z(7-bwph^7n0#h=@ylwf=&f45%=d&t5w1(&pxxg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhileNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'stranded.urls'
@@ -134,3 +136,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_TMP=os.path.join(BASE_DIR,'static')
+STATIC_URL='/static/'
+
+os.makedirs(STATIC_TMP,exist_ok=True)
+os.makedirs(STATIC_ROOT,exist_ok=True)
+
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+)
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
